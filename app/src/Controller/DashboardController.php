e6cf9a0bab79a20259040d575dd13ef7c184fb30
Alex Dean <alexandre.dean@ext.pichet.com>
2023-01-02 10:46:03 +0100

    Building dashboard and game details page

---
+++ app/src/Controller/DashboardController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Country;
+use App\Entity\Game;
+use App\Service\GameService;
+use DateInterval;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine) {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        $user           = $this->getUser();
+        $countries      = $this->doctrine->getRepository(Country::class)->findAll();
+        $gameRepository = $this->doctrine->getRepository(Game::class);
+        $flagGames      = $gameRepository->findBy(
+            [
+                'player' => $user->getId(),
+                'state'  => GameService::GAME_STATE_FINISHED
+            ]
+        );
+
+        $bestScore     = 0;
+        $totalPoints   = 0;
+        $totalGameTime = 0;
+
+        foreach ($flagGames as $flagGame) {
+            $score = count($countries) - count($flagGame->getForgottenCountries());
+            $totalPoints += $score;
+            $totalGameTime += $this->getTotalSeconds($flagGame->getDuration());
+
+            if (true === $score > $bestScore) {
+                $bestScore = $score;
+            }
+        }
+
+        $averageGameTime = $totalGameTime / $totalPoints;
+
+        return $this->render('dashboard.html.twig', [
+            'average_score_time' => number_format((float)$averageGameTime, 2, '.', ''),
+            'best_score'         => $bestScore,
+            'countries'          => $countries,
+            'flag_games'         => array_reverse($flagGames),
+            'user'               => $user
+        ]);
+    }
+
+    #[Route('/game/{gameId}', name: 'app_game_details')]
+    public function gameDetails($gameId): Response
+    {
+        $user           = $this->getUser();
+        $countries      = $this->doctrine->getRepository(Country::class)->findAll();
+        $gameRepository = $this->doctrine->getRepository(Game::class);
+        $game           = $gameRepository->findOneBy(
+            [
+                'id'     => $gameId,
+                'player' => $user->getId()
+            ]
+        );
+
+        if (GameService::GAME_TYPE_FLAGS === $game->getType()) {
+            return $this->flagGameDetails($countries, $game);
+        }
+
+        $response = new Response();
+        $response->setContent('Game not found');
+        return $response;
+    }
+
+    private function getTotalSeconds(DateInterval $duration)
+    {
+        return $duration->s + $duration->i * 60 + $duration->h * 3600;
+    }
+
+    private function flagGameDetails(array $countries, Game $game): Response
+    {
+        $totalGameTime   = $this->getTotalSeconds($game->getDuration());
+        $score           = count($countries) - count($game->getForgottenCountries());
+        $averageGameTime = $totalGameTime / $score;
+
+        return $this->render('flag-game-details.html.twig', [
+            'game' => $game,
+            'score' => $score,
+            'average_score_time' => number_format((float)$averageGameTime, 2, '.', ''),
+            'countries' => $countries,
+            'forgotten_countries' => $game->getForgottenCountries(),
+        ]);
+    }
+}
